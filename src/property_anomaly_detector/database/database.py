8c37639a99f527b1d858ce959b258c2f414cc443
Marco Cardoso <marcom.c@outlook.com>
2020-07-19 18:56:41 +0100

    Update database.py module

--- src/property_anomaly_detector/database/database.py
+++ src/property_anomaly_detector/database/database.py
@@ -10,7 +10,7 @@ class Database:
         Database constructor
         :param database_name: A string with the database_name
         """
-        self.client = MongoClient(os.environ.get('MONGO_HOST'), int( os.environ.get('MONGO_PORT')))
+        self.client = MongoClient(os.environ.get('MONGO_HOST'), int(os.environ.get('MONGO_PORT')))
 
         database = self.client[database_name]
 
@@ -19,8 +19,8 @@ class Database:
         self.districts = database['districts']
         self.errors = database["errors"]
 
-    def get_properties(self, default_projection={}):
-        return list(self.properties.find({}, default_projection))
+    def get_properties(self, default_filter={}, projection={}):
+        return list(self.properties.find(default_filter, projection))
 
     def insert_properties(self, properties: list):
         """

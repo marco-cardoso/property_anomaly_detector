e04967afab02ae8b1d81a7426e356bc349990c49
Marco Cardoso <marcom.c@outlook.com>
2020-08-19 10:14:15 -0700

    Update save_ld_district_names.py

--- src/property_anomaly_detector/datasets/save_ld_district_names.py
+++ src/property_anomaly_detector/datasets/save_ld_district_names.py
@@ -2,6 +2,7 @@
     This module reads the london_district_names.csv file and
     stores it into a MongoDB collection
 """
+import os
 import logging
 
 import pandas as pd
@@ -19,9 +20,10 @@ logging.basicConfig(
 
 def main():
     db = Database("zoopla")
-    file = pd.read_csv(
-        "london_district_names.csv"
-    )
+
+    script_path = os.path.dirname(__file__)
+    csv_path = os.path.join(script_path, "london_district_names.csv")
+    file = pd.read_csv(csv_path)
 
     logging.info("Inserting districts into MongoDB collection")
     london_districts = list(file['district_name'].values)
